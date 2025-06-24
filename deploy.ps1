727347317006c0efa95b10402b5aa683871ff166
idolaman <idolav9@gmail.com>
2025-06-24 15:10:15 +0300

    Fully deployment over kubernetes

--- deploy.ps1
+++ deploy.ps1
@@ -1,27 +1,23 @@
-# Deploy to Minikube script (PowerShell)
-Write-Host "Deploying crawler services to Minikube..." -ForegroundColor Green
+# Deploy to Docker Desktop Kubernetes script (PowerShell)
+Write-Host "Deploying crawler services to Docker Desktop Kubernetes..." -ForegroundColor Green
 
-# Build Docker images with Minikube's Docker daemon
-Write-Host "Setting up Minikube Docker environment..." -ForegroundColor Yellow
-& minikube docker-env --shell powershell | Invoke-Expression
+# Ensure we are using the docker-desktop context
+Write-Host "Switching kubectl context to 'docker-desktop'..." -ForegroundColor Yellow
+kubectl config use-context docker-desktop | Out-Null
 
-# Build service1
-Write-Host "Building service1..." -ForegroundColor Yellow
-docker build -t crawler/service1:latest ./service1/
+# Build Docker images (Docker Desktop shares daemon with its Kubernetes)
+Write-Host "Building service1 image..." -ForegroundColor Yellow
+docker build -t crawler/service1:latest -f service1/Dockerfile .
 
-# Build service2
-Write-Host "Building service2..." -ForegroundColor Yellow
-docker build -t crawler/service2:latest ./service2/
+Write-Host "Building service2 image..." -ForegroundColor Yellow
+docker build -t crawler/service2:latest -f service2/Dockerfile .
 
 # Deploy using Helm
 Write-Host "Deploying with Helm..." -ForegroundColor Yellow
 helm upgrade --install crawler ./helm-chart/
 
 Write-Host "Deployment complete!" -ForegroundColor Green
-Write-Host ""
-Write-Host "To access service2 externally:" -ForegroundColor Cyan
-Write-Host "minikube service crawler-service2 --url" -ForegroundColor White
-Write-Host ""
-Write-Host "Or get the NodePort URL:" -ForegroundColor Cyan
-$minikubeIP = "localhost"
-Write-Host "http://${minikubeIP}:30080" -ForegroundColor White 
+Write-Host "" 
+Write-Host "Service #2 (NodePort) should be reachable at:" -ForegroundColor Cyan
+$nodePort = 30080
+Write-Host "http://localhost:$nodePort" -ForegroundColor White 
--- helm-chart/templates/_helpers.tpl
+++ helm-chart/templates/_helpers.tpl
@@ -2,28 +2,28 @@
 Expand the name of the chart.
 */}}
 {{- define "crawler.name" -}}
-{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+    {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Create a default fully qualified app name.
 */}}
 {{- define "crawler.fullname" -}}
-{{- if .Values.fullnameOverride }}
-{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
-{{- else }}
-{{- $name := default .Chart.Name .Values.nameOverride }}
-{{- if contains $name .Release.Name }}
-{{- .Release.Name | trunc 63 | trimSuffix "-" }}
-{{- else }}
-{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
-{{- end }}
-{{- end }}
+    {{- if .Values.fullnameOverride }}
+        {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+    {{- else }}
+        {{- $name := default .Chart.Name .Values.nameOverride }}
+    {{- if contains $name .Release.Name }}
+        {{- .Release.Name | trunc 63 | trimSuffix "-" }}
+    {{- else }}
+        {{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+        {{- end }}
+    {{- end }}
 {{- end }}
 
 {{/*
 Create chart name and version as used by the chart label.
 */}}
 {{- define "crawler.chart" -}}
-{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+    {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }} 
